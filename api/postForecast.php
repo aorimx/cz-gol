fb3ba32fee4c7abf5cf1dff3a886eab2f491f006
Marco Gómez <marco.gomez@outlook.com>
2017-11-30 13:00:46 -0600

    save data

--- api/postForecast.php
+++ api/postForecast.php
@@ -13,12 +13,12 @@ if ($conn->connect_error) {
 
 for ($i=1; $i <= 4; $i++) {
     $itemName="partido".$i;
-    //$sql = "INSERT INTO predicciones(id_partido,id_usuario,resultado) values ('".$i."','".$_POST['user']."','".$_POST[$itemName]."')";
-    //$result = $conn->query($sql);
-    /*if ($conn->query($sql) === TRUE) {
+    $sql = "INSERT INTO predicciones(id_partido,id_usuario,resultado) values ('".$i."','".$_POST['user']."','".$_POST[$itemName]."')";
+    $result = $conn->query($sql);
+    if ($conn->query($sql) === TRUE) {
         
     } else {
-    }*/
+    }
 }
 $conn->close();
 
